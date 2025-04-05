e9bc983b9e7ecaeae141f9c91694435e96e4bd88
Юля Забытина <user@mail.ru>
2025-04-05 20:19:07 +0300

    Добавлено объявление класса tvector

--- My_individual_project/tvector.h
+++ My_individual_project/tvector.h
@@ -1,25 +1,31 @@
 #pragma once
 enum State {
-	emty, busy, deleted
+	empty, busy, deleted
 };
 template <class T>
 class Tvector {
 	T* _data;
 	size_t _capacity;
 	size_t _size;
-	size_t deleted;
-	State* states;
-	inline bool is_full() const noexcept;
+	size_t _deleted;
+	State* _states;
+	inline bool is_full() const noexcept {
+		return _size == _capacity;
+	}
+
 public:
-	Tvector(sie_t size = 0);
+	Tvector();
 	Tvector(size_t size);
 	Tvector(T* data);
-	Tvector(const Tvector<T>& vector);
+	Tvector(const Tvector<T>& other_vector);
+
 	~Tvector();
 
 	inline bool is_empty() const noexcept;
 	inline T& front() noexcept;
 	inline T* begin() noexcept;
-	inline T* data() noexcept; //сеттер
-	inline const T* data() const noexcept; //геттер
+	inline T* data(const T* new_data, size_t new_size) noexcept; 
+	inline const T* data() const noexcept; 
 };
+
+
