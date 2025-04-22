2ff406cdd29b8b15ce5195df5bc40083cbf92865
Юлия <uzabytina9@gmail.com>
2025-04-22 19:11:47 +0300

    Delete passport_data.h

--- passport_data.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class Passport {
-	int  _passport_number;
-	int  _passport_series;
-public:
-	Passport(int passport_number, int passport_series): _passport_number(passport_number), _passport_series (passport_series) {}
-};
