a3a45942301104bbe976fa76ce700c58d4df808d
Юлия <uzabytina9@gmail.com>
2025-04-22 19:12:48 +0300

    Delete post.h

--- post.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-class Post {
-	int _salary;
-	std::string _post_name;
-public:
-	Post(int salary, const std::string& post_name): _salary(salary), _post_name(post_name) {}
-};
