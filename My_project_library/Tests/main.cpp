c98802a8c335d41b3fd9ee085f83e74637142623
Юля Забытина <user@mail.ru>
2025-04-27 23:02:00 +0300

    k

---
+++ My_project_library/Tests/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <windows.h>
+#include <exception>
+#include <stdexcept>
+#include "../Tvector_class/tvector.h"
+
+void set_color(int text_color, int bg_color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, (bg_color << 4) | text_color);
+}
+namespace TestSystem {
+    int count_success = 0, count_failed = 0;
+    void start_test(bool(*test)(), const char* name_of_test) {
+        set_color(2, 0);
+        std::cout << "[ RUN      ] ";
+        set_color(7, 0);
+        std::cout << name_of_test << std::endl;
+
+        bool status = test();
+
+        if (status == true) {
+            set_color(2, 0);
+            std::cout << "[       OK ]" << std::endl;
+            count_success++;
+        }
+        else {
+            set_color(4, 0);
+            std::cout << "[  FAILED  ]" << std::endl;
+            count_failed++;
+        }
+        set_color(7, 0);
+    }
+    template <class T>
+    bool check(const T& expected, const T& actual) {
+        if (expected == actual) {
+            return true;
+        }
+        else {
+            std::cerr << "Expected result is " << expected << ", but actual is " << actual << "." << std::endl;
+            return false;
+        }
+    }
+    void print_init_info() {
+        set_color(2, 0);
+        std::cout << "[==========] " << std::endl;
+        set_color(7, 0);
+    }
+    void print_final_info() {
+        set_color(2, 0);
+        std::cout << "[==========] ";
+        set_color(7, 0);
+        std::cout << count_success + count_failed << " test" << (count_success + count_failed > 1 ? "s" : "") << " ran." << std::endl;
+        set_color(2, 0);
+        std::cout << "[  PASSED  ] ";
+        set_color(7, 0);
+        std::cout << count_success << " test" << (count_success > 1 ? "s" : "") << std::endl;
+        if (count_failed > 0) {
+            set_color(4, 0);
+            std::cout << "[  FAILED  ] ";
+            set_color(7, 0);
+            std::cout << count_failed << " test" << (count_failed > 1 ? "s." : ".") << std::endl;
+        }
+    }
+};
+
+bool test_1() {
+    return true;
+}
+
+int main() {
+    TestSystem::start_test(test_1, " ");
+
+    TestSystem::print_final_info();
+    system("pause");
+    return 0;
+}
+
