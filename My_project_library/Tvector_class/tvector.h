6508abdb129839663d7479f749cdf62f012273e6
Юля Забытина <user@mail.ru>
2025-04-27 22:53:51 +0300

    Добавлены все изменения

--- My_project_library/Tvector_class/tvector.h
+++ My_project_library/Tvector_class/tvector.h
@@ -57,9 +57,77 @@ public:
 	inline T* end() const noexcept {
 		return _data + _size;
 	}
+
+	void pop_front_elem() {
+		int first_not_null = 0;
+		while (mass[first_not_null] == NULL) {
+			first_not_null++;
+		}
+		mass[first_not_null] = NULL;
+	}
+	void pop_front_elems(int* mass, int size, int count) {
+		int first_not_null = 0;
+		for (int i = 0; i < count; i++) {
+			while (mass[first_not_null] == NULL) {
+				first_not_null++;
+			}
+			mass[first_not_null] = NULL;
+		}
+	}
+	void pop_back_elem(int* mass, int size) {
+		int last_not_null = size - 1;
+		while (mass[last_not_null] == NULL) {
+			last_not_null--;
+		}
+		mass[last_not_null] = NULL;
+	}
+	void pop_back_elems(int* mass, int size, int count) {
+		int last_not_null = size - 1;
+		for (int i = 0; i < count; i++) {
+			while (mass[last_not_null] == NULL) {
+				last_not_null--;
+			}
+			mass[last_not_null] = NULL;
+		}
+	}
+	void erase_elem(int* mass, int size, int pos) {
+		int step = 0;
+		for (int i = 0; i < size; i++) {
+			if (mass[i] != NULL) {
+				step++;
+
+			}
+			if (step == pos) {
+				mass[i] = NULL;
+				break;
+			}
+		}
+	}
+	void erase_elems(int* mass, int size, int pos, int count) {
+		int step = 0;
+		for (int i = 0; i < size; i++) {
+			if (mass[i] != NULL) {
+				step++;
+			}
+			if (step == pos) {
+				mass[i] = NULL;
+				if (pos != count) {
+					pos++;
+				}
+			}
+
+		}
+	}
+	/*
+	
+	 Стандартные функции вставки: push_front(), push_back(), insert().
+
+  
+  Функция замены значения emplace().
+	
+	*/
 };
 /*
 	friend void sort_array(Tvector<T>& vector);
 	friend void shuffle_array(Tvector<T>& vector);
 */
-
