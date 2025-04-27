0e1d68de2314f4627bd9bcc05f02b1a5b52beb00
Юля Забытина <user@mail.ru>
2025-04-27 19:06:18 +0300

    В .h файл добавлена реализация inline-методов (сеттеры, геттеры)

--- My_project_library/Tvector_class/tvector.h
+++ My_project_library/Tvector_class/tvector.h
@@ -13,7 +13,6 @@ class Tvector {
 	inline bool is_full() const noexcept {
 		return _size == _capacity;
 	}
-
 public:
 	Tvector() noexcept;
 	Tvector(size_t size);
@@ -21,16 +20,46 @@ public:
 	Tvector(const Tvector<T>& other_vector);
 	~Tvector() noexcept;
 
-	void sort_array(Tvector<T>& vector);
+	inline bool is_empty() const noexcept {
+		return _size == 0;
+	}
 
-	inline bool is_empty() const noexcept;
-	size_t size() const noexcept;
-	size_t capacity() const noexcept;
+	inline const T* data() const noexcept {
+		return _data;
+	}
+	inline T* data() noexcept {
+		return _data;
+	}
 
-	inline T& back() noexcept;
-	inline T& front() noexcept;
-	inline T* begin() noexcept;
-	inline T* end() noexcept;
-	inline T* data(const T* new_data, size_t new_size) noexcept;
-	inline const T* data() const noexcept;
+	inline T& front() noexcept {
+		return _data[0];
+	}
+	inline T& back() noexcept { 
+		return _data[_size - 1];
+	}
+	inline const T& front() noexcept {
+		return _data[0];
+	}
+	inline const T& back() noexcept {
+		return _data[_size - 1];
+	}
+
+	size_t size() const noexcept {
+		return _size;
+	}
+	size_t capacity() const noexcept {
+		return _capacity;
+	}
+
+	inline T* begin() const noexcept {
+		return _data;
+	}
+	inline T* end() const noexcept {
+		return _data + _size;
+	}
 };
+/*
+	friend void sort_array(Tvector<T>& vector);
+	friend void shuffle_array(Tvector<T>& vector);
+*/
+
