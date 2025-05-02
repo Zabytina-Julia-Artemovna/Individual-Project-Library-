adaa9f854a8db3465a09138dbb789ef6bf7023e7
Юлия <uzabytina9@gmail.com>
2025-05-02 18:26:26 +0300

    Delete My_project_library/Library/Library_classes directory

--- My_project_library/Library/Library_classes/post.cpp
+++
@@ -1,9 +0,0 @@
-#include "post.h"
-Post::Post(std::string name_of_post, int salary) noexcept : _name_of_post(name_of_post), _salary(salary) {}
-Post::Post(const Post& other_post) noexcept : _name_of_post(other_post._name_of_post), _salary(other_post._salary) {}
-int Post::get_salary() const noexcept {
-	return _salary;
-}
-std::string& Post::get_name_of_post() noexcept {
-	return _name_of_post;
-}
