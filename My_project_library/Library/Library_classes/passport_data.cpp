a686983a96c830928402c883d380d1980bb4b80f
Юля Забытина <user@mail.ru>
2025-05-02 16:54:03 +0300

    Добавлена реализация 'независимых' классов для Библиотеки

---
+++ My_project_library/Library/Library_classes/passport_data.cpp
@@ -0,0 +1,9 @@
+#include "passport_data.h"
+Passport::Passport(int number, int series) : _number(number), _series(series) {}
+Passport::Passport(const Passport& other_passport) : _number(other_passport._number), _series(other_passport._series) {}
+int Passport::get_number() const noexcept {
+	return _number;
+}
+int Passport::get_series() const noexcept {
+	return _series;
+}
