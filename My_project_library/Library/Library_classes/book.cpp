56c04c403f53da6e6e3b4c4802de979a9a0a20c5
Юля Забытина <user@mail.ru>
2025-05-02 18:06:20 +0300

    Обновлены book.cpp book.h post.h

--- My_project_library/Library/Library_classes/book.cpp
+++ My_project_library/Library/Library_classes/book.cpp
@@ -1,9 +1,13 @@
+//Copyright 2025 Zabytina Julia
 #include "book.h"
-Book::Book(int count, int year_of_publication, int id, std::string title, std::string author, std::string publishing_house,
+Book::Book(int count, int year_of_publication, int id, std::string title, std::string author,
+	std::string publishing_house,
 	std::string language, State state) noexcept : _count(count), _year_of_publication(year_of_publication), _id(id),
 	_title(title), _author(author), _publishing_house(publishing_house), _language(language), _state(state) {}
-Book::Book(const Book& other_book) noexcept : _count(other_book._count), _year_of_publication(other_book._year_of_publication), _id(other_book._id),
-_title(other_book._title), _author(other_book._author), _publishing_house(other_book._publishing_house),
+Book::Book(const Book& other_book) noexcept : _count(other_book._count), 
+_year_of_publication(other_book._year_of_publication), _id(other_book._id),
+_title(other_book._title), _author(other_book._author), 
+_publishing_house(other_book._publishing_house),
 _language(other_book._language), _state(other_book._state) {}
 int Book::get_count() const noexcept {
 	return _count;
