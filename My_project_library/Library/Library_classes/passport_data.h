2809b89faaf170ade5981266f1569cadcd5a9720
Юля Забытина <user@mail.ru>
2025-05-21 19:31:02 +0300

    Небольшие правки

--- My_project_library/Library/Library_classes/passport_data.h
+++ My_project_library/Library/Library_classes/passport_data.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef> 
 class Passport {
 private:
     size_t _passport_number;
@@ -7,8 +8,7 @@ public:
     Passport() : _passport_number(0), _passport_series(0) {}
     Passport(size_t number, size_t series) : _passport_number(number),
         _passport_series(series) {}
-    Passport(const Passport& other) : _passport_number(other._passport_number),
-        _passport_series(other._passport_series) {}
+    Passport(const Passport& other) = default;
     size_t get_number() const {
         return _passport_number;
     }
