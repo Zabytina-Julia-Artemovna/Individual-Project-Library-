98d36d7397846c1c2c6bf5c65a11e40a267b3e49
Юля Забытина <user@mail.ru>
2025-05-21 19:54:30 +0300

    Небольшие оптимизационные правки

--- My_project_library/Library/Library_classes/fio.h
+++ My_project_library/Library/Library_classes/fio.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-
 class FIO {
 private:
     std::string _last_name;
@@ -8,29 +7,31 @@ private:
     std::string _first_name;
 
 public:
-    FIO() : _last_name("Ivanov"), _name("Ivan"), _first_name("Ivanovich") {}
-    FIO(std::string name, std::string last_name, std::string first_name) :
-        _last_name(last_name), _name(name), _first_name(first_name) {}
-    FIO(const FIO& other) :
-        _last_name(other._last_name),
-        _name(other._name),
-        _first_name(other._first_name) {}
-    std::string get_name() const {
+    FIO(): 
+        _last_name(), 
+        _name(), 
+        _first_name() {}
+    FIO(const std::string& name, const std::string& last_name, const std::string& first_name):
+        _last_name(last_name), 
+        _name(name), 
+        _first_name(first_name) {}
+    FIO(const FIO& other) = default;
+    const std::string& get_name() const {
         return _name;
     }
-    std::string get_last_name() const {
+    const std::string& get_last_name() const {
         return _last_name;
     }
-    std::string get_first_name() const {
+    const std::string& get_first_name() const {
         return _first_name;
     }
-    void set_name(std::string name) {
+    void set_name(const std::string& name) {
         _name = name;
     }
-    void set_last_name(std::string last_name) {
+    void set_last_name(const std::string& last_name) {
         _last_name = last_name;
     }
-    void set_first_name(std::string first_name) {
+    void set_first_name(const std::string& first_name) {
         _first_name = first_name;
     }
 };
