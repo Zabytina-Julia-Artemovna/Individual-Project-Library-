983c56b45e735a060ee23c6011050bc9f46444df
Юля Забытина <user@mail.ru>
2025-05-21 19:25:26 +0300

    Небольшие оптимизационные правки

--- My_project_library/Library/Library_classes/post.h
+++ My_project_library/Library/Library_classes/post.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <cstddef> 
 class Post {
 private:
     std::string _post_name;
@@ -7,17 +8,16 @@ private:
 
 public:
     Post() : _post_name("Unknown"), _salary(0) {}
-    Post(std::string post_name, size_t salary) : _post_name(post_name),
+    Post(const std::string& post_name, size_t salary) : _post_name(post_name),
         _salary(salary) {}
-    Post(const Post& other) : _post_name(other._post_name),
-        _salary(other._salary) {}
-    std::string get_post_name() const {
+    Post(const Post& other) = default;
+    const std::string& get_post_name() const {
         return _post_name;
     }
     size_t get_salary() const {
         return _salary;
     }
-    void set_post_name(std::string post_name) {
+    void set_post_name(const std::string& post_name) {
         _post_name = post_name;
     }
     void set_salary(size_t salary) {
