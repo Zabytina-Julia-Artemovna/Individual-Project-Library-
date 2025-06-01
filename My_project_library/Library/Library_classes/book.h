3cd2cd6c3c26604366ebb5831b58cbf9bba96f83
Юля Забытина <user@mail.ru>
2025-06-01 11:21:10 +0300

    За ненадобностью убраны состояния книги

--- My_project_library/Library/Library_classes/book.h
+++ My_project_library/Library/Library_classes/book.h
@@ -3,23 +3,18 @@
 #include <cstddef> 
 class Book {
 private:
-    enum State {
-        on_hands, reserved, free
-    };
     std::string _book_id;
     size_t _year_of_publication;
     std::string _author;
     std::string _language;
     std::string _publishing_house;
     std::string _book_name;
-    State _current_state;
 public:
     Book(size_t year_of_publication,
         const std::string& author, const std::string& language,
         const std::string& publishing_house, const std::string& book_name) :
         _book_id(book_name + "_" + author),
         _year_of_publication(year_of_publication),
-        _current_state(free),
         _author(author),
         _language(language),
         _publishing_house(publishing_house),
@@ -43,9 +38,6 @@ public:
     const std::string& get_book_name() const {
         return _book_name;
     }
-    State get_state() const {
-        return _current_state;
-    }
     void set_year_of_publication(size_t year_of_publication) {
         _year_of_publication = year_of_publication;
     }
@@ -60,9 +52,6 @@ public:
     }
     void set_book_name(const std::string& book_name) {
         _book_name = book_name;
-    }
-    void set_state(State state) {
-        _current_state = state;
     }
 };
 
