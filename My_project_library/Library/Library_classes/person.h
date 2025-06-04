4438ec485ca2fe1175fadb23436a9111ca55c119
Юля Забытина <user@mail.ru>
2025-06-04 11:10:52 +0300

    Добавлен person.h

---
+++ My_project_library/Library/Library_classes/person.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include "fio.h"
+#include "passport_data.h"
+class Person {
+private:
+    std::string _phone_number;
+    std::string _date_of_birth;
+    FIO _fio;
+    Passport _passport_data;
+public:
+
+};
